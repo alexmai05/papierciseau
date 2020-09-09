63395a72d90ce7485a8eac323d42d9cb4e65adef
alexmai05 <55108815+alexmai05@users.noreply.github.com>
2020-09-09 16:34:13 -0400

    Update javascript.js

--- javascript.js
+++ javascript.js
@@ -3,4 +3,42 @@ const jouer = document.querySelector(".jouer");
 jouer.addEventListener("click", () => {
     document.querySelector(".boxFormulaire").classList.add("none");
     document.querySelector(".fondFormulaire").classList.add("none");
+    document.querySelector(".formulaire").classList.add("none");
 })
+
+class Game{
+    constructor() {
+
+        this.afficherNomJoueur();
+
+        this.listArmes = [
+            ["✊", "✋", "✌️"]
+            ["✊🏻", "✋🏻", "✌🏻"]
+            ["✊🏽", "✋🏽", "✌🏽"]
+            ["✊🏿", "✋🏿", "✌🏿"]
+        ];
+
+        this.afficherChoixJoueur();
+      }
+
+      afficherNomJoueur(){
+        const nomFormulaire = document.querySelector("nomTexte").value;
+        const nomEnJeu = document.querySelector("nomJoueur");
+        nomEnJeu.innerHTML = nomFormulaire;
+      }
+
+      afficherChoixJoueur(){
+          const choixPeauList = document.querySelector("[name = choixPeau]:checked").value;
+
+          const choixArmeList = document.querySelector("[name = armes]");
+          const choixArme = choixArmeList.value;
+
+          const couleurPeauJoueur = this.listArmes[choixPeauList];
+          const armeJoueur = couleurPeauJoueur[choixArme];
+
+          const resultatJoueur = document.querySelector(".emojiJoueur");
+          resultatJoueur.innerHTML = armeJoueur;
+       
+      }
+}
+
